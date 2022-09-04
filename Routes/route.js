4f529698afdab8c52f6fc6eb82f8fb7dc2557bfc
pratinilopant <mr.pratiknilopant@gmail.com>
2022-09-04 18:12:12 +0530

    v1.0.0

--- Routes/route.js
+++ Routes/route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { userSignup, userLogIn } from "../Controller/user-controller.js";
-import { getProducts, getProductById } from '../controller/product-controller.js';
+import { getProducts, getProductById } from '../Controller/product-controller.js';
 import { addPaymentGateway } from '../controller/payment-controller.js';
 
 const router = express.Router();
--- server.js
+++ server.js
@@ -44,6 +44,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'callback'
+paytmParams['CALLBACK_URL'] = 'https://flipkartmernclone.herokuapp.com/callback'
 paytmParams['EMAIL'] = 'pratiknilopant98@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
